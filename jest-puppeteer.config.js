95c3cede0d4548d0fed2e7e46f7e4cc7a9a370eb
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-08-05 20:32:19 -0400

    add basic functionality tests

---
+++ jest-puppeteer.config.js
@@ -0,0 +1,16 @@
+const path = require('path')
+
+const CRX_PATH = path.resolve(__dirname, './tmp/chrome')
+
+module.exports = {
+    launch: {
+        dumpio: true,
+        headless: false,
+        product: 'chrome',
+        args: [
+            `--disable-extensions-except=${CRX_PATH}`,
+            `--load-extension=${CRX_PATH}`
+        ],
+    },
+    browserContext: 'default',
+}
